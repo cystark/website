a494b6f6a726ba34aec5bcf62c2f2b90619fa790
CYStark <cam@cystark.com.au>
2019-06-06 10:19:40 +1000

    CTA component

---
+++ src/components/CTA/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import styles from "./styles.module.scss"
+import { joinFilter } from "../../utils/helpers"
+
+interface Props {
+  className: string
+}
+
+const CTA: React.FunctionComponent<Props> = ({ children, className }) => {
+  const styleOptions = [styles.container, className]
+
+  return <div className={joinFilter(styleOptions)}>{children}</div>
+}
+
+export default CTA
