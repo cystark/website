3305d4226759f5988abcd8bb3ae9ac1745b24743
CYStark <cam@cystark.com.au>
2019-06-07 12:14:48 +1000

    Allow for bottom padding on Title component

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -2,24 +2,30 @@ import React from "react"
 import styles from "./styles.module.scss"
 import textStyle from "../../styles/text.module.scss"
 import { textAlignOptions } from "../../type/options"
+import { joinFilter, capitalizeWord } from "../../utils/helpers"
 
 interface Props {
   className?: string
   children: string
+  bottomPadding: string
 }
 
 const Title: React.FunctionComponent<Props & textAlignOptions> = ({
   children,
   className,
   textAlign = "left",
+  bottomPadding = "normal",
 }) => {
   const styleOptions = [
     className && className,
     styles.title,
-    textAlign && textStyle[textAlign],
+    bottomPadding
+      ? styles[`bottomPadding${capitalizeWord(bottomPadding)}`]
+      : "",
+    textStyle ? textStyle[textAlign] : "",
   ]
-  console.log(styleOptions)
-  return <h3 className={styleOptions.join(" ")}>{children}</h3>
+
+  return <h3 className={joinFilter(styleOptions)}>{children}</h3>
 }
 
 export default Title
