fac61fe3b3367d756d909aed39b69ad31c1c9719
CYStark <cam@cystark.com.au>
2019-06-07 12:13:47 +1000

    CTA styling and syntax modification

--- src/components/CTA/index.tsx
+++ src/components/CTA/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 const CTA: React.FunctionComponent<Props> = ({ children, className }) => {
-  const styleOptions = [styles.container, className]
+  const styleOptions = [styles.cta, className]
 
   return <div className={joinFilter(styleOptions)}>{children}</div>
 }
