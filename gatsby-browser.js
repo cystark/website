7c5fc115b7977678934a63bc573d8c2487bc0331
CYStark <cam@cystark.com.au>
2019-07-16 09:05:14 +1000

    portal document error fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,13 @@
-import "./src/styles/colors.scss"
-import "./src/styles/global.scss"
+import "@styles/colors.scss"
+import "@styles/global.scss"
 import wrapWithProvider from "./wrap-with-provider"
 
 export const wrapRootElement = wrapWithProvider
 
 // Loading Portal
-const portal = document.createElement("div")
-portal.id = "modal-root"
-document.body.appendChild(portal)
 
-
+if (typeof document !== "undefined") {
+  const portal = document.createElement("div")
+  portal.id = "modal-root"
+  document.body.appendChild(portal)
+}
