e4f4c3b06b8636c33b73a3779515f1fc7dfcf1fb
CYStark <cam@cystark.com.au>
2019-08-14 13:13:03 +1000

    Addition of MDX plugins for blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,34 @@
-const config = require("./config/site")
+const config = require('./config/site');
 
 module.exports = {
   siteMetadata: {
     ...config,
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-typescript",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-typescript',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: config.title,
         short_name: config.shortName,
         description: config.description,
         start_url: config.pathPrefix,
-        display: "standalone",
+        display: 'standalone',
         icon: config.favicon,
       },
     },
     {
-      resolve: "gatsby-plugin-web-font-loader",
+      resolve: 'gatsby-plugin-web-font-loader',
       options: {
         google: {
-          families: ["Oswald", "Lato", "Cabin Condensed"],
+          families: ['Oswald', 'Lato', 'Cabin Condensed'],
         },
       },
     },
     {
-      resolve: "gatsby-plugin-react-svg",
+      resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
           include: /images/, // See below to configure properly
@@ -36,11 +36,87 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: `${__dirname}/src/pages/`,
+        path: `${__dirname}/content/blog`,
+        name: 'blog',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        extensions: ['.mdx', '.md', '.markdown'],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              backgroundColor: '#fafafa',
+              maxWidth: 1035,
+            },
+          },
+        ],
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-catch-links',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'GOOGLE_ID',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => allMdx.edges.map(edge => Object.assign({}, edge.node.frontmatter, {
+              description: edge.node.excerpt,
+              date: edge.node.fields.date,
+              url: `${site.siteMetadata.siteUrl}/${edge.node.fields.slug}`,
+              guid: `${site.siteMetadata.siteUrl}/${edge.node.fields.slug}`,
+            })),
+            query: `
+              {
+                allMdx(
+                  limit: 1000,
+                  filter: { frontmatter: { published: { ne: false } } }
+                  sort: { order: DESC, fields: [frontmatter___date] }
+                ) {
+                  edges {
+                    node {
+                      excerpt(pruneLength: 250)
+                      fields { 
+                        slug
+                        date
+                      }
+                      frontmatter {
+                        title
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+            title: 'CYStark Blog Feed',
+          },
+        ],
       },
     },
   ],
-}
+};
